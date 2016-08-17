fa125069af345470c1339ea06f574671fb600271
Liam Newman <bitwiseman@gmail.com>
2016-08-17 16:34:53 -0700

    Test

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -52,24 +52,24 @@ public class GoogleExampleIT extends DriverBase {
         // Create a new WebDriver instance
         // Notice that the remainder of the code relies on the interface,
         // not the implementation.
-        WebDriver driver = getDriver();
-
-        // And now use this to visit Google
-        driver.get("http://192.168.1.3:8080");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
-
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.id("comma-first"));
-
-        // Enter something to search for
-        element.click();
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
+        // WebDriver driver = getDriver();
+        //
+        // // And now use this to visit Google
+        // driver.get("http://192.168.1.3:8080");
+        // // Alternatively the same thing can be done like this
+        // // driver.navigate().to("http://www.google.com");
+        //
+        // // Find the text input element by its name
+        // WebElement element = driver.findElement(By.id("comma-first"));
+        //
+        // // Enter something to search for
+        // element.click();
+        //
+        // // Now submit the form. WebDriver will find the form for us from the element
+        // element.submit();
+        //
+        // // Check the title of the page
+        // System.out.println("Page title is: " + driver.getTitle());
         //
         // // Google's search is rendered dynamically with JavaScript.
         // // Wait for the page to load, timeout after 10 seconds
