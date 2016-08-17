c08be0a0b08fa97daf5f22ff8d801d3c036e9e9c
Liam Newman <bitwiseman@gmail.com>
2016-08-17 16:42:24 -0700

    Test

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -22,18 +22,18 @@ public class GoogleExampleIT extends DriverBase {
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
 
-        // // Find the text input element by its name
-        // WebElement element = driver.findElement(By.name("q"));
-        //
-        // // Enter something to search for
-        // element.clear();
-        // element.sendKeys("Cheese!");
-        //
-        // // Now submit the form. WebDriver will find the form for us from the element
-        // element.submit();
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
 
-        // // Check the title of the page
-        // System.out.println("Page title is: " + driver.getTitle());
+        // Enter something to search for
+        element.clear();
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
         //
         // // Google's search is rendered dynamically with JavaScript.
         // // Wait for the page to load, timeout after 10 seconds
