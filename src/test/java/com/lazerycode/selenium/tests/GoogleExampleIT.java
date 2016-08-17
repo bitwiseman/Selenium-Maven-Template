e4c2b033339ce0986f70f6106506abe59d69ee96
Liam Newman <bitwiseman@gmail.com>
2016-08-16 18:08:59 -0700

    test

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -22,15 +22,15 @@ public class GoogleExampleIT extends DriverBase {
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
-        element.clear();
-        element.sendKeys("Cheese!");
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
+        // // Find the text input element by its name
+        // WebElement element = driver.findElement(By.name("q"));
+        //
+        // // Enter something to search for
+        // element.clear();
+        // element.sendKeys("Cheese!");
+        //
+        // // Now submit the form. WebDriver will find the form for us from the element
+        // element.submit();
 
         // // Check the title of the page
         // System.out.println("Page title is: " + driver.getTitle());
